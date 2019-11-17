3c7d72e3daf9eb9985a0aad342134ed8cf8bf72d
nokchoa <camilonovoam@gmail.com>
2019-11-17 01:58:34 -0500

    nueva ruta para empleado técnico para un nuevo host

--- cliente/src/app/app-routing.module.ts
+++ cliente/src/app/app-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
         {path: 'perfil', component: PerfilClienteComponent },
         {path: 'pqr', component: PqrClienteComponent}
       ]},
-  { path: 'empleado', component: EmpleadoNHostComponent},
+  { path: 'empleado/nhost', component: EmpleadoNHostComponent},
 ];
 
 @NgModule({
