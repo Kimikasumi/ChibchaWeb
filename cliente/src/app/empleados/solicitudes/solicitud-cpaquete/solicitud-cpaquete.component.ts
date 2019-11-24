89fe5e533543caa38369a120c8fead5ee2dc4846
nokchoa <camilonovoam@gmail.com>
2019-11-24 17:28:19 -0500

    terminado gran parte del modulo, faltan correciones

--- cliente/src/app/empleados/solicitudes/solicitud-cpaquete/solicitud-cpaquete.component.ts
+++ cliente/src/app/empleados/solicitudes/solicitud-cpaquete/solicitud-cpaquete.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+
+export interface Paquete {
+  cod_paquete: number;
+  nom_paquete: string;
+}
 
 @Component({
   selector: 'app-solicitud-cpaquete',
@@ -7,7 +13,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SolicitudCpaqueteComponent implements OnInit {
 
-  constructor() { }
+  paquetes: Paquete[] = [
+    {cod_paquete: 1, nom_paquete: 'Chibcha-plata'},
+    {cod_paquete: 2, nom_paquete: 'Chibcha-oro'},
+    {cod_paquete: 3, nom_paquete: 'Chibcha-platino'}
+  ];
+
+  nombreCliente = 'pedro';
+  planPago = 'Anual';
+  descripcion = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci animi architecto atque consectetur\n' +
+    '        consequatur debitis, dolor esse nobis officiis, provident quasi rem similique tempore voluptatibus? Nisi\n' +
+    '        provident quidem quos.\n';
+
+  constructor(private router: Router) {
+  }
+
+  enviar() {
+    this.router.navigate(['empleado/solicitudes']);
+  }
 
   ngOnInit() {
   }
