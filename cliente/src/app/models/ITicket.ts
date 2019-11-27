9d284495e424ed8fbafe1bdfcb4719f7c76ee09c
nokchoa <camilonovoam@gmail.com>
2019-11-27 00:43:47 -0500

    Auto stash before merge of "master" and "origin/master"

--- cliente/src/app/models/ITicket.ts
+++ cliente/src/app/models/ITicket.ts
@@ -10,13 +10,3 @@ export interface ITicketC {
   cod_estado?: string;
   nom_estado?: string;
 }
-
-export interface ITicketS{
-    cod_dominio?:number;
-    cod_t_ticket?:number;
-    nom_dominio?:string;
-    nombre?:string;
-    descripcion?:string;
-    nom_estado?:string;
-    cedula?:number;
-}
